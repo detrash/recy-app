e3782c5590b05b29afc552ce734a344e868d14b4
Yuri Mutti <muttiyuri@gmail.com>
2024-06-12 17:18:28 -0300

    feat: lefthook

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Signin from "@/pages/signin";
-import Dashboard from "@/pages/dashboard";
-import { Toaster } from "@/components/ui/toaster";
-import { Providers } from "./components/providers";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Signin from '@/pages/signin';
+import Dashboard from '@/pages/dashboard';
+import { Toaster } from '@/components/ui/toaster';
+import { Providers } from './components/providers';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Signin />,
   },
   {
-    path: "/dashboard",
+    path: '/dashboard',
     element: <Dashboard />,
   },
   {
-    path: "/settings",
+    path: '/settings',
     element: <Dashboard />,
   },
   {
-    path: "/submit",
+    path: '/submit',
     element: <Dashboard />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
       <RouterProvider router={router} />
       <Toaster />
     </Providers>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
