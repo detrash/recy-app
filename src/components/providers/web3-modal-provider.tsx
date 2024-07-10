667c3cb2b1da71bea443c6a0e3726abc8aad8d91
Yuri Mutti <muttiyuri@gmail.com>
2024-07-10 19:02:45 -0300

    feat: remove text and add translate

--- src/components/providers/web3-modal-provider.tsx
+++ src/components/providers/web3-modal-provider.tsx
@@ -1,14 +1,14 @@
-import { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createWeb3Modal } from "@web3modal/wagmi/react";
-import { State, WagmiProvider } from "wagmi";
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createWeb3Modal } from '@web3modal/wagmi/react';
+import { State, WagmiProvider } from 'wagmi';
 
-import { projectId, wagmiConfig } from "@/config";
+import { projectId, wagmiConfig } from '@/config/wagmi';
 
 // Setup queryClient
 const queryClient = new QueryClient();
 
-if (!projectId) throw new Error("Project ID is not defined");
+if (!projectId) throw new Error('Project ID is not defined');
 
 // Create modal
 createWeb3Modal({
--- src/modules/calculator/screens/main/index.tsx
+++ src/modules/calculator/screens/main/index.tsx
@@ -16,11 +16,11 @@ export default function CalculatorScreen() {
     <>
       <section className="flex justify-center bg-[url('/assets/bg/ocean-dark.jpg')] bg-cover bg-center p-6 text-center text-white">
         <div className="border-md flex flex-col gap-4 p-2">
-          <p className="max-w-96 text-lg">{t('calculator:home_hero')}</p>
+          <p className="max-w-96 text-lg">{t('calculator.main.hero')}</p>
 
           <div>
             <Button size="lg" onClick={handleAcquireCertificate}>
-              {t('calculator:home_cta')}
+              {t('calculator.main.cta')}
             </Button>
           </div>
         </div>
@@ -29,44 +29,30 @@ export default function CalculatorScreen() {
       <div className="container mx-auto my-6 flex max-w-2xl flex-col gap-6">
         <Card className="sm:rounded-xl sm:shadow-xl">
           <CardHeader className="flex flex-row items-center gap-4 pb-2">
-            <h2 className="text-lg lg:text-2xl">Transforming waste management</h2>
+            <h2 className="text-lg lg:text-2xl">{t('calculator.main.paragraph.one.title')}</h2>
           </CardHeader>
           <CardContent>
-            <p className="text-sm lg:text-base">
-              We need to build a world free from waste pollution. We need it for our own sake. For
-              nature's sake. We already have over 300 million tons of trash in our environment. 14
-              million new tons getting to our oceans every year on average. Today's solutions,
-              despite very important, is mostly focused on getting trash out of the oceans and
-              rivers and not preventing waste to get to nature.
-            </p>
+            <p className="text-sm lg:text-base">{t('calculator.main.paragraph.one.text')}</p>
           </CardContent>
         </Card>
 
         <Card className="sm:rounded-xl sm:shadow-xl">
           <CardHeader className="flex flex-row items-center gap-4 pb-2">
-            <h2 className="text-lg lg:text-2xl">Lets&apos;s Clean Together</h2>
+            <h2 className="text-lg lg:text-2xl">{t('calculator.main.paragraph.two.title')}</h2>
           </CardHeader>
 
           <CardContent>
-            <p className="text-sm lg:text-base">
-              The Recy certificate represents climate compensation through financially supporting
-              actors that conducts recycling, composting and/or other sustainable and circular waste
-              treatment activities in different regions of the world.
-            </p>
+            <p className="text-sm lg:text-base">{t('calculator.main.paragraph.two.text')}</p>
           </CardContent>
         </Card>
 
         <Card className="sm:rounded-xl sm:shadow-xl">
           <CardHeader className="flex flex-row items-center gap-4 pb-2">
-            <h2 className="text-lg lg:text-2xl">About DeTrash</h2>
+            <h2 className="text-lg lg:text-2xl">{t('calculator.main.paragraph.three.title')}</h2>
           </CardHeader>
 
           <CardContent>
-            <p className="text-sm lg:text-base">
-              DeTrash is the RECY Network founder and main contributor. DeTrash offers tech based
-              solutions to clean our world. DeTrash's vision is that a world without waste pollution
-              is not only possible but it's the best alternative to a more equitable and free world.
-            </p>
+            <p className="text-sm lg:text-base">{t('calculator.main.paragraph.three.text')}</p>
           </CardContent>
         </Card>
       </div>
