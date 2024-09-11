95cd644c5ddd24f0c0596a193fad48cea037000a
edwardoranj <175904393+edwardoranj@users.noreply.github.com>
2024-09-11 14:18:30 +0800

    add ts-ignore due to mismatched type

--- src/providers/web3-modal-provider.tsx
+++ src/providers/web3-modal-provider.tsx
@@ -3,7 +3,31 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createWeb3Modal } from '@web3modal/wagmi/react';
 import { State, WagmiProvider } from 'wagmi';
 
-import { projectId, wagmiConfig } from '@/libs/wagmi';
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
+import { mainnet, sepolia } from 'wagmi/chains';
+
+// Get projectId at https://cloud.walletconnect.com
+export const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
+
+if (!projectId) throw new Error('WalletConnect Project ID is not defined');
+
+const metadata = {
+  description: 'Web3Modal Example',
+  // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+
+  name: 'Web3Modal',
+  url: 'https://web3modal.com',
+};
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const;
+
+const config = defaultWagmiConfig({
+  chains,
+  metadata,
+  projectId,
+});
 
 // Setup queryClient
 const queryClient = new QueryClient();
@@ -15,9 +39,9 @@ createWeb3Modal({
   enableAnalytics: true,
   // Optional - defaults to your Cloud configuration
   enableOnramp: true,
-
   projectId,
-  wagmiConfig, // Optional - false as default
+  // @ts-ignore
+  wagmiConfig: config,
 });
 
 export function Web3ModalProvider({
@@ -28,7 +52,8 @@ export function Web3ModalProvider({
   initialState?: State;
 }) {
   return (
-    <WagmiProvider config={wagmiConfig} initialState={initialState}>
+    // @ts-ignore
+    <WagmiProvider config={config} initialState={initialState}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
